f499bc9617a03d4f09ad1edd9398d58b49998699
Sebastian Aguirre Duque <sadw621@gmail.com>
2022-11-24 15:57:53 -0500

    Create and clean app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+/* Author: Sebastian Aguirre Duque
+E-mail: sadw621@gmail.com */
+
+import Button from '@mui/material/Button';
+
+import './App.scss';
 
 function App() {
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Button variant="outlined">Hello World</Button>
+
     </div>
+
   );
+
 }
 
 export default App;
