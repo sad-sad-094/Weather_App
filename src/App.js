cc956cb107ada5bdbcdf3eea674fc1d17166c64f
Sebastian Aguirre Duque <sadw621@gmail.com>
2022-11-24 17:26:47 -0500

    BuildingApp

--- src/App.js
+++ src/App.js
@@ -1,17 +1,166 @@
 /* Author: Sebastian Aguirre Duque
 E-mail: sadw621@gmail.com */
 
+import { useState } from 'react';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Container from '@mui/material/Container';
+
 
 import './App.scss';
+import Navbar from './Components/Modules/Navbar';
+
 
 function App() {
+
+  const [expanded, setExpanded] = useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
 
     <div className="App">
 
-      <Button variant="outlined">Hello World</Button>
+      <Navbar />
+
+      <Container maxWidth="sm">
+
+        <Box
+          component="form"
+          sx={{
+            '& > :not(style)': { m: 1, width: '50%' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+
+          <TextField id="standard-basic" label="City" variant="standard" size="small" fullWidth color="secondary" helperText="Please enter a city" />
+        </Box>
+
+        <Button variant="outlined">Search</Button>
+
+        <Card>
+          <CardContent>
+            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+              Word of the Day
+            </Typography>
+            <Typography variant="h5" component="div">
+              benevolent
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              adjective
+            </Typography>
+            <Typography variant="body2">
+              well meaning and kindly.
+              <br />
+              {'"a benevolent smile"'}
+            </Typography>
+          </CardContent>
+        </Card>
+
+
+        <Box
+          component="span"
+          sx={{
+            width: '85%', justifyContent: 'center', alignItems: 'center'
+          }}>
+          <Accordion expanded={expanded === 'panel1'} TransitionProps={{ unmountOnExit: true }} onChange={handleChange('panel1')}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1bh-content"
+              id="panel1bh-header"
+            >
+              <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                General settings
+              </Typography>
+              <Typography sx={{ color: 'text.secondary' }}>I am an accordion</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
+                Aliquam eget maximus est, id dignissim quam.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion expanded={expanded === 'panel2'} TransitionProps={{ unmountOnExit: true }} onChange={handleChange('panel2')}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2bh-content"
+              id="panel2bh-header"
+            >
+              <Typography sx={{ width: '33%', flexShrink: 0 }}>Users</Typography>
+              <Typography sx={{ color: 'text.secondary' }}>
+                You are currently not an owner
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus,
+                varius pulvinar diam eros in elit. Pellentesque convallis laoreet
+                laoreet.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion expanded={expanded === 'panel3'} TransitionProps={{ unmountOnExit: true }} onChange={handleChange('panel3')}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel3bh-content"
+              id="panel3bh-header"
+            >
+              <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                Advanced settings
+              </Typography>
+              <Typography sx={{ color: 'text.secondary' }}>
+                Filtering has been entirely disabled for whole web server
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
+                amet egestas eros, vitae egestas augue. Duis vel est augue.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion expanded={expanded === 'panel4'} TransitionProps={{ unmountOnExit: true }} onChange={handleChange('panel4')}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel4bh-content"
+              id="panel4bh-header"
+            >
+              <Typography sx={{ width: '33%', flexShrink: 0 }}>Personal data</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
+                amet egestas eros, vitae egestas augue. Duis vel est augue.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+
+        </Box>
+
+      </Container>
+
 
     </div>
 
